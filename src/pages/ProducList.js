021cfb8816bfee67d6633e4e34a7861925d7d924
Lucas X <lucasmxisto@outlook.com>
2023-02-09 16:11:59 -0300

    Requisito 2 completo

---
+++ src/pages/ProducList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class ProductList extends React.Component {
+  render() {
+    return (
+      <div>
+        <input />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
